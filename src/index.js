0b44bc7549918d4357cad7efcc51fb9eb6f4ca06
Oleksandr Levchuk <emihrant.ua@gmail.com>
2023-03-29 20:56:54 +0200

    user register, login, logout, refresh

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
             <App />
           </BrowserRouter>
         </PersistGate>
