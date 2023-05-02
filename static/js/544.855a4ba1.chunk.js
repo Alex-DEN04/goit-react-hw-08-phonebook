10548d47acbc2b3dbe94c068e090fe997bb2048d
Alex-DEN04 <emihrant.ua@gmail.com>
2023-05-02 09:52:03 +0000

    Deploying to gh-pages from @ Alex-DEN04/goit-react-hw-08-phonebook@ee032d68dc6980b2efd42d19213f77b148682240 🚀

---
+++ static/js/544.855a4ba1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{9544:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var o=t(176),i=t(9229),c=t(184);function r(){return(0,c.jsx)(o.k,{justify:"center",p:15,children:(0,c.jsx)(i.x,{as:"h1",fontFamily:"heading",fontSize:"2xl",fontWeight:"bold",justifyContent:"center",children:"Welcome"})})}}}]);
+//# sourceMappingURL=544.855a4ba1.chunk.js.map
